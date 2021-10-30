6f03c3b1a86c2beb607aa3c0ebbf377c35950279
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-10-30 12:14:41 +0800

    split it to two stacks

---
+++ cdk/lib/backend-stack.ts
@@ -0,0 +1,54 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda-go";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import { CorsHttpMethod, HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2";
+import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
+
+export class BackendStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const table = new dynamodb.Table(this, "picker-table", {
+      partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    const fatLambda = new lambda.GoFunction(this, "handler", {
+      entry: "../backend/go/cmd/fat-lambda",
+      environment: {
+        table: table.tableName,
+        region: this.region,
+        GIN_MODE: "release",
+      },
+    });
+
+    table.grantReadWriteData(fatLambda);
+
+    const httpApi = new HttpApi(this, "api-gateway", {
+      corsPreflight: {
+        allowMethods: [
+          CorsHttpMethod.GET,
+          CorsHttpMethod.HEAD,
+          CorsHttpMethod.OPTIONS,
+          CorsHttpMethod.POST,
+        ],
+        allowOrigins: ["http://localhost:3000"],
+        maxAge: cdk.Duration.minutes(10),
+      },
+    });
+
+    httpApi.addRoutes({
+      path: "/{proxy+}",
+      methods: [HttpMethod.ANY],
+      integration: new LambdaProxyIntegration({
+        handler: fatLambda,
+      }),
+    });
+
+    new cdk.CfnOutput(this, "httpGateway", {
+      value: httpApi.apiEndpoint,
+      description: "This needs to go into the frontend .env",
+    });
+  }
+}
--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -1,42 +1,11 @@
 import * as cdk from "@aws-cdk/core";
-import * as lambda from "@aws-cdk/aws-lambda-go";
-import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
-import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2";
-import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
 
-export class CdkStack extends cdk.Stack {
+export class FrontendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const table = new dynamodb.Table(this, "picker-table", {
-      partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
-      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-    });
-
-    const fatLambda = new lambda.GoFunction(this, "handler", {
-      entry: "../backend/go/cmd/fat-lambda",
-      environment: {
-        table: table.tableName,
-        region: this.region,
-        GIN_MODE: "release",
-      },
-    });
-
-    table.grantReadWriteData(fatLambda);
-
-    const httpApi = new HttpApi(this, "api-gateway", {});
-
-    httpApi.addRoutes({
-      path: "/{proxy+}",
-      methods: [HttpMethod.ANY],
-      integration: new LambdaProxyIntegration({
-        handler: fatLambda,
-      }),
-    });
 
     const svelteBucket = new s3.Bucket(this, "svelte-bucket", {
       websiteIndexDocument: "index.html",
@@ -65,6 +34,9 @@ export class CdkStack extends cdk.Stack {
     );
 
     new s3deploy.BucketDeployment(this, "static-svelte-website-deployment", {
+      /**
+       * Invalidate the index.html on every deploy
+       */
       distribution,
       distributionPaths: ["/index.html"],
       sources: [
@@ -77,8 +49,8 @@ export class CdkStack extends cdk.Stack {
               "bash",
               "-c",
               [
-                "pnpm install",
-                `VITE_API_URL=${httpApi.apiEndpoint} pnpm run build`,
+                `pnpm install`,
+                "pnpm run build",
                 "cp -r /asset-input/build/* /asset-output/",
               ].join(" && "),
             ],
@@ -89,11 +61,7 @@ export class CdkStack extends cdk.Stack {
     });
 
     new cdk.CfnOutput(this, "cloudfrontDistribution", {
-      value: `https://${distribution.domainName}`,
-    });
-
-    new cdk.CfnOutput(this, "httpGateway", {
-      value: httpApi.apiEndpoint,
+      value: `https://${distribution.distributionDomainName}`,
     });
   }
 }
