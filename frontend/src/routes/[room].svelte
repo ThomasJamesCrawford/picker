ac6515fc3243e71ead88d3c852365db272a96f9e
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-11-01 23:20:50 +0800

    style the select page

--- frontend/src/routes/[room].svelte
+++ frontend/src/routes/[room].svelte
@@ -24,17 +24,38 @@
 	import type { PublicRoom } from '$lib/types/Room';
 
 	export let room: PublicRoom;
+
+	let selectedOption = null;
 </script>
 
-<div class="card shadow-lg">
-	<div class="card-title">
-		{room.question}
-	</div>
-	<div class="card-body">
-		{#each room.options as option}
-			<button type="button" class="btn btn-secondary">
-				{option}
-			</button>
-		{/each}
-	</div>
+<div class="container mx-auto max-w-lg py-4">
+	<form class="card shadow-lg">
+		<div class="card-body">
+			<div class="card-title">
+				{room.question}
+			</div>
+			<div class="flex flex-col space-y-2">
+				{#each room.options as option, i}
+					<button
+						class:btn-active={selectedOption === i}
+						class:white={selectedOption === i}
+						on:click={() => (selectedOption = i)}
+						type="button"
+						class="btn btn-secondary btn-outline border-2"
+					>
+						{option}
+					</button>
+				{/each}
+			</div>
+			<div class="flex justify-end mt-4">
+				<button class="btn btn-primary">Save</button>
+			</div>
+		</div>
+	</form>
 </div>
+
+<style lang="postcss">
+	.white {
+		@apply text-white;
+	}
+</style>
