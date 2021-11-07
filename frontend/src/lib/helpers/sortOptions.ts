62f9f4e76769281c01c3a4f8e503a3eda13c242a
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-11-07 14:05:32 +0800

    use generic

--- frontend/src/lib/helpers/sortOptions.ts
+++ frontend/src/lib/helpers/sortOptions.ts
@@ -1,6 +1,6 @@
-import type { PublicOption } from '$lib/types/Room';
+import type { Option, PublicOption } from '$lib/types/Room';
 
-export const sortOptions = (options: PublicOption[]): PublicOption[] =>
+export const sortOptions = <T extends PublicOption | Option>(options: T[]): T[] =>
 	options.sort((a, b) => {
 		if (a.value === b.value) {
 			return a.id.localeCompare(b.id);
