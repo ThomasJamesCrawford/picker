0a99fa7f5f8cdde80effb4be396f6fc1797a9e87
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-11-07 15:24:13 +0800

    allow adding new options to existing

--- frontend/src/routes/admin/[room].svelte
+++ frontend/src/routes/admin/[room].svelte
@@ -23,6 +23,7 @@
 <script lang="ts">
 	import { sortOptions } from '$lib/helpers/sortOptions';
 	import Info from '$lib/icons/info.svelte';
+	import Plus from '$lib/icons/plus.svelte';
 	import Trash from '$lib/icons/trash.svelte';
 	import type { Option, Room } from '$lib/types/Room';
 
@@ -32,6 +33,9 @@
 
 	let deleteLoading: string | null = null;
 	let error = '';
+
+	let addLoading = false;
+	let optionInputValue = '';
 
 	const deleteOption = async (optionID: string, roomID: string) => {
 		deleteLoading = optionID;
@@ -57,6 +61,36 @@
 			})
 			.finally(() => {
 				deleteLoading = null;
+			});
+	};
+
+	const addOption = async (option: string, roomID: string) => {
+		error = '';
+		addLoading = true;
+
+		if (!option) return;
+
+		return fetch(`${import.meta.env.VITE_API_URL}/room/${roomID}/option`, {
+			method: 'POST',
+			headers: {
+				accepts: 'application/json'
+			},
+			body: JSON.stringify({
+				option
+			})
+		})
+			.then((res) => res.json())
+			.then((res: Option) => {
+				room = {
+					...room,
+					options: sortOptions([...room.options, res])
+				};
+			})
+			.catch(() => {
+				error = 'Something went wrong, try refreshing the page.';
+			})
+			.finally(() => {
+				addLoading = false;
 			});
 	};
 </script>
@@ -135,10 +169,47 @@
 						</div>
 					{/if}
 				{/each}
-				{#if error}
-					<div class="alert alert-warning mt-4">{error}</div>
-				{/if}
 			</div>
+			<div class="form-control my-2">
+				<label for="option" class="label justify-start space-x-2">
+					<span class="label-text">Add another option</span>
+					<span
+						data-tip="Add another option"
+						class="text-gray-500 tooltip-bottom tooltip tooltip-accent"
+					>
+						<Info />
+					</span>
+				</label>
+				<div class="flex space-x-2">
+					<input
+						id="option"
+						on:keydown={(e) => {
+							if (e.key === 'Enter') {
+								addOption(optionInputValue, room.id);
+								e.preventDefault();
+								e.stopPropagation();
+							}
+						}}
+						bind:value={optionInputValue}
+						type="text"
+						placeholder="6:00 am Friday 25/06/21"
+						class="w-full input input-bordered"
+					/>
+					<button
+						on:click={() => addOption(optionInputValue, room.id)}
+						type="button"
+						class:loading={addLoading}
+						class="btn btn-secondary btn-circle"
+					>
+						{#if !addLoading}
+							<Plus />
+						{/if}
+					</button>
+				</div>
+			</div>
+			{#if error}
+				<div class="alert alert-warning mt-4">{error}</div>
+			{/if}
 		</div>
 	</div>
 </div>
